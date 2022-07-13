d6fa4fe2a2c1b76218c3dab4234e518bbeca62fd
100daniel <48659486+100daniel@users.noreply.github.com>
2022-07-13 15:42:10 -0500

    Create 1_2.php

---
+++ 1_2.php
@@ -0,0 +1,7 @@
+<p>El video que te presento en esta lección te ayudará a comprender qué es y para qué sirve el Design Thinking.</p>
+
+<p>Está basado en mi experiencia de años de trabajo en desarrollo de proyectos, las clases de la maestría de diseño ambiental en la Universidad de Calgary, múltiples documentos y el curso de design thinking del MIT.</p>
+
+<p>Antes de empezar te invito a reflexionar.</p>
+
+<p>¿Crees que todos tenemos capacidad para resolver problemas complejos de una manera distinta a la que siempre los abordamos?</p>
