7c32321e2eb2eb2cc98450b0387f7e1ac705fb13
100daniel <48659486+100daniel@users.noreply.github.com>
2022-07-13 12:47:19 -0500

    Create 1_4.php

---
+++ 1_4.php
@@ -0,0 +1,42 @@
+
+<h2>Ejercicio: Practica el proceso de design thinking</h2>
+
+<p>El<span>objetivo del ejercicio es poner en práctica el proceso de<em>design thinking</em></span>de forma ágil, por lo que se establecen tiempos cortos. Aunque puedes tomarte todo el tiempo que quieras.<br><span>Recuerda que se trata de un proceso iterativo, por lo que puedes regresar a los pasos anteriores o inclusive hacer algunos pasos de manera simultánea.</span></p><p><span style="letter-spacing: -0.24px;">Cuando apliques el proceso</span><em style="letter-spacing: -0.24px;">design thinking</em><span style="letter-spacing: -0.24px;">a tus proyectos, establece tiempos más largos y trabaja en sesiones de al menos una o dos horas para ir avanzando. Inclusive podrás pasar días en una misma etapa.</span></p><p><span style="letter-spacing: -0.24px;">Lo importante es ir avanzando e idealmente implementando de manera continua durante el proceso.</span></p><p><span style="letter-spacing: -0.24px;">Para hacer el ejercicio, primero, repasa rápidamente los pasos a seguir del ejercicio y luego sigue las instrucciones de cada paso para ir trabajando en una propuesta.</span></p>
+
+<h2>Instrucciones del ejercicio para aplicar el proceso de design thinking:</h2>
+<figure class="wp-block-image size-large" style="box-sizing: inherit; margin: 0px 0px 1em; color: #604685; font-family: 'SF UI Text', sans-serif; font-size: 19px; letter-spacing: -0.24px; background-color: #fffdfc;"><img class="wp-image-97948 ls-is-cached lazyloaded" style="box-sizing: inherit; border: 0px; font-style: italic; height: auto; max-width: 100%; vertical-align: bottom;" src="https://identidadydesarrollo.com/wp-content/uploads/2019/07/Design-thinking-diagrama-horizontal-1024x350.png" sizes="(max-width: 1024px) 100vw, 1024px" srcset="https://identidadydesarrollo.com/wp-content/uploads/2019/07/Design-thinking-diagrama-horizontal-1024x350.png 1024w, https://identidadydesarrollo.com/wp-content/uploads/2019/07/Design-thinking-diagrama-horizontal-300x102.png 300w, https://identidadydesarrollo.com/wp-content/uploads/2019/07/Design-thinking-diagrama-horizontal-768x262.png 768w, https://identidadydesarrollo.com/wp-content/uploads/2019/07/Design-thinking-diagrama-horizontal-1536x525.png 1536w, https://identidadydesarrollo.com/wp-content/uploads/2019/07/Design-thinking-diagrama-horizontal-400x137.png 400w, https://identidadydesarrollo.com/wp-content/uploads/2019/07/Design-thinking-diagrama-horizontal-600x205.png 600w, https://identidadydesarrollo.com/wp-content/uploads/2019/07/Design-thinking-diagrama-horizontal.png 1880w" alt="Qué es el diagrama horizontal de design thinking" width="1024" height="350" data-src="https://identidadydesarrollo.com/wp-content/uploads/2019/07/Design-thinking-diagrama-horizontal-1024x350.png" data-srcset="https://identidadydesarrollo.com/wp-content/uploads/2019/07/Design-thinking-diagrama-horizontal-1024x350.png 1024w, https://identidadydesarrollo.com/wp-content/uploads/2019/07/Design-thinking-diagrama-horizontal-300x102.png 300w, https://identidadydesarrollo.com/wp-content/uploads/2019/07/Design-thinking-diagrama-horizontal-768x262.png 768w, https://identidadydesarrollo.com/wp-content/uploads/2019/07/Design-thinking-diagrama-horizontal-1536x525.png 1536w, https://identidadydesarrollo.com/wp-content/uploads/2019/07/Design-thinking-diagrama-horizontal-400x137.png 400w, https://identidadydesarrollo.com/wp-content/uploads/2019/07/Design-thinking-diagrama-horizontal-600x205.png 600w, https://identidadydesarrollo.com/wp-content/uploads/2019/07/Design-thinking-diagrama-horizontal.png 1880w"></figure>
+
+<ul>
+<li>Paso 1 – Empatiza: Selecciona tu usuario (5 minutos), prepárate y realiza una entrevista (20-25 minutos)</li>
+<li>Paso 2 – Define: Selecciona un problema específico (10-15 minutos)</li>
+<li>Paso 3 – Idea: Genera y evalúa distintas ideas para resolver el problema (20 minutos)</li>
+<li>Paso 4 – Prototipa : Genera un prototipo de solución (10-15 minutos)</li>
+<li>Paso 5 – Prueba: Obtén comentarios e iteraciones del usuario (15-20 minutos)</li>
+<li>Paso 6 – Retroalimenta: Ajusta tu prototipo (15 – 20 minutos).</li>
+</ul>
+<h2>Pasos a seguir para generar una innovación a través del ejercicio de design thinking</h2>
+<h3 >Paso 1 – Empatiza (25 – 30 minutos)</h3>
+<p>Idealmente, ya diseñaste a tu buyer persona en el ejercicio del curso de definición de un cliente ideal, así que este punto te será muy fácil.</p>
+<p>Si hiciste el ejercicio completo muy probablemente habrás trabajado en los pasos 2 y 3 de este ejercicio.</p>
+<h3>1.1 Selecciona un usuario con quien trabajar (5 minutos)</h3>
+<p>Si ya realizaste tu ejercicio de construcción del perfil de cliente ideal o buyer persona, puedes usar ese mismo usuario.</p>
+<h3>1.2  Prepárate y realiza una breve entrevista (20-25 minutos)</h3>
+<p>Escribe una lista de preguntas abiertas que te permitirán profundizar en las necesidades de tu usuario. Por ejemplo ¿Cómo te informas cuando vas a visitar un lugar? ¿Qué es lo más importante cuando eliges un lugar para comer fuera de tu lugar de residencia?</p>
+<h3>Paso 2 – Define. Selecciona un problema específico (10-15 minutos)</h3>
+<p>Entre las distintas necesidades o problemas a los que se enfrenta tu usuario, selecciona uno específico en el que vas a trabajar. Por ejemplo, dificultad para encontrar información sobre un destino, poca agilidad de los medios para reservar, accesibilidad limitada, o algo más que le represente potencialmente complejo durante su experiencia con tu producto o servicio.</p>
+<p>Si el problema que estás seleccionando es demasiado grande es posible que te cueste mucho trabajo generar una solución. Si es el caso, recuerda partir el problema en otros más pequeños y explora cuales podrían tener un impacto mayor para contribuir a solucionar el problema principal.</p>
+<h3>Paso 3 – Idea: Genera y evalúa distintas ideas para resolver el problema</h3>
+<p>(20 minutos)</p>
+<p>Haz una rápida lluvia de ideas, anota todas las que te vengan a la mente y organízalas.</p>
+<h3>Paso 4 – Prototipa: Genera un prototipo de solución (10-15 minutos)</h3>
+<p>Realiza un dibujo de tu propuesta de solución al problema enfocándote en los principales elementos o funcionalidades para satisfacer la necesidad del usuario que entrevistaste de una manera creativa.</p>
+<h3>Paso 5 – Prueba: Obtén comentarios e iteraciones de posibles usuarios (15-20 minutos)</h3>
+<p>Lleva tu propuesta de solución al cliente potencial o actual presentando tu prototipo (en este caso el dibujo de la propuesta de solución) a quien entrevistaste en el paso 1.2 del ejercicio.</p>
+<p>Procura hacer preguntas abiertas y escuchar. Recuerda que no estás “vendiendo” tu solución, sino que quieres ponerla a prueba y obtener retroalimentación para mejorarla.</p>
+<p>Si tienes oportunidad, realiza ajustes a tu prototipo durante la conversación y valida nuevamente si con esos ajustes se mejora la experiencia.</p>
+<p>Anota tus observaciones y comentarios para que más adelante puedas hacer los ajustes que no puedas resolver de manera inmediata.</p>
+<h3>Paso 6. Ajusta tu prototipo (20 minutos)</h3>
+<p>Realiza las iteraciones que sean necesarias hasta tener un prototipo con el que sientas comodidad para hacer un lanzamiento al mercado.</p>
+<p><span style="box-sizing: inherit; font-weight: bolder;">Tip pro:</span>Algunos expertos dicen que si no te da un poco de pena el primer producto con el que saliste al mercado entonces no lo lanzaste a tiempo. Es decir, a veces la oportunidad puede ser más importante que la perfección. Puedes ir haciendo ajustes conforme vayas teniendo retroalimentación de tus clientes reales.</p>
+<h3 ><span >Créditos</span></h3>
+<p>Este ejercicio fue diseñado tomando en cuenta las recomendaciones del curso de Design Thinking del MIT impartido por múltiples expertos en diseño en la plataforma de Coursera, registrado bajo licencia Creative Commons 4.0 y fue adaptado para facilitar el desarrollo de tu proyecto con algunos ajustes para enfocar el desarrollo de proyectos turísticos y culturales.</p>
