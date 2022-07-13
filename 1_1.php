d73413d4021b05b188e24e8f8fd90c249e22046c
100daniel <48659486+100daniel@users.noreply.github.com>
2022-07-13 15:32:29 -0500

    Create 1_1.php

---
+++ 1_1.php
@@ -0,0 +1,40 @@
+<h2>Bienvenida a la lección de Pensamiento de Diseño (Design thinking)</2>
+
+<p>Las organizaciones y proyectos se enfrentan constantemente con la necesidad de innovar y diversificar sus productos y servicios para agregar valor a sus clientes actuales y potenciales.</p>
+
+<p>Independientemente del medio en el que te desempeñes, seguramente tendrás interés en innovar y muy probablemente habrás escuchado sobre el concepto de design thinking.</p>
+
+      
+<h2>Crea soluciones con pensamiento de diseño</h2>
+ 
+<p>Con este curso comenzarás a comprender los conceptos y pasos del design thinking o pensamiento de diseño, como un proceso iterativo que te permitirá avanzar de manera progresiva en el desarrollo de soluciones para agregar valor a través de la resolución de necesidades y resolver problemas.</p>
+
+<p>Este enfoque te ayudará a adquirir habilidades valiosas tanto para el ámbito personal como para la vida personal.</p>
+
+   
+<h3>Objetivos de aprendizaje</h3>
+<p>En este curso de design thinking buscamos ayudarte a cumplir con los siguientes objetivos:</p>
+
+<h3>Conocimientos</h3>
+ 
+<p>Conceptos de diseño y el proceso de design thinking.</p>
+
+<h3>Habilidades</h3>
+ 
+<p>Resolución de problemas de manera creativa.</p>
+
+<h3>Actitudes</h3>
+ 
+<p>Creatividad, flexibilidad y persistencia.</p>
+
+ 
+<h2>Duración del curso</h2>
+<p>Esta lección tiene una duración aproximada de 4 horas de estudio.</p>
+
+<p>Los contenidos estarán disponibles por un tiempo indefinido, para que puedas consultarlos cuando quieras.</p>
+
+                                   
+                                   <h3>Alexandro Dupuis</h3>
+
+
+<p>Director de Innovación y marketing Digital de Identidad y Desarrollo, es especialista en gestión de proyectos e inbound marketing.Ha colaborado con diversas organizaciones e instituciones públicas y privadas en el diseño, operación y seguimiento de políticas, programas y proyectos de desarrollo sostenible y marketing digital.</p>
